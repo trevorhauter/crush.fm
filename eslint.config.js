95e77ba28c7e5ee7d31ee0a56d281d658afb7cc1
Trevor <trevor@hauter.dev>
2025-03-12 22:39:32 -0400

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
-import js from '@eslint/js'
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -7,11 +8,25 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      // Remove ...tseslint.configs.recommended and replace with this
+      ...tseslint.configs.recommendedTypeChecked,
+      // Alternatively, use this for stricter rules
+      ...tseslint.configs.strictTypeChecked,
+      // Optionally, add this for stylistic rules
+      ...tseslint.configs.stylisticTypeChecked,
+      reactX.configs.recommended,
+      reactDom.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
+    
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       'react-hooks': reactHooks,
@@ -23,6 +38,8 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      ...reactX.configs['recommended-typescript'].rules,
+      ...reactDom.configs.recommended.rules,
     },
   },
 )
